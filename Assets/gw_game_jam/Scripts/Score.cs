2754b9d31f6141471fb5850cfa4ba4f5945feb42
Fumihito Morita <adringonion.byazura@gmail.com>
2021-05-02 16:29:19 +0900

    スコア用Monobehaviorと値オブジェクト追加

---
+++ Assets/gw_game_jam/Scripts/Score.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace gw_game_jam.Scripts
+{
+    public class Score
+    {
+        private readonly int _value;
+
+        public int  Value => this._value;
+
+
+        public Score(int value)
+        {
+            this._value = value;
+        }
+
+        public Score add(int addScore)
+        {
+            return new Score(_value + addScore);
+        }
+
+        public Score remove(int removeScore)
+        {
+            return new Score(_value - removeScore);
+        }
+    }
+}
--- Assets/gw_game_jam/Scripts/ScoreController.cs
+++ Assets/gw_game_jam/Scripts/ScoreController.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ScoreController : MonoBehaviour
+namespace gw_game_jam.Scripts
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ScoreController : MonoBehaviour
     {
+        // Start is called before the first frame update
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
     }
 }
