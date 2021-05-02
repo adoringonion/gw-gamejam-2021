d03e6c2c4adfb7f420139302b192d881542dde3b
Fumihito Morita <adringonion.byazura@gmail.com>
2021-05-02 16:57:45 +0900

    スペースキーで10点入るように

--- Assets/gw_game_jam/Scripts/ScoreController.cs
+++ Assets/gw_game_jam/Scripts/ScoreController.cs
@@ -1,19 +1,31 @@
-using System;
 using UnityEngine;
 
 namespace gw_game_jam.Scripts
 {
     public class ScoreController : MonoBehaviour
     {
+        private ScoreInteractor _scoreInteractor;
+        
+        [SerializeField]
+        public int currentScore;
+        
         private void Awake()
         {
-            
+            _scoreInteractor = new ScoreInteractor();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
-        
+            ScoreInputTest();
+            currentScore = _scoreInteractor.ScoreForView();
+        }
+
+        private void ScoreInputTest()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _scoreInteractor.addScoreWhenEnemyKilled();
+            }
         }
     }
 }
--- Assets/gw_game_jam/Scripts/ScoreRepository.cs
+++ Assets/gw_game_jam/Scripts/ScoreRepository.cs
@@ -2,14 +2,11 @@
 {
     public class ScoreRepository
     {
-
-        private Score _inmemoryScore;
-
         public Score InmemoryScore { get; set; }
 
         public ScoreRepository()
         {
-            this._inmemoryScore = new Score(0);
+            this.InmemoryScore = new Score(0);
         }
 
     }
