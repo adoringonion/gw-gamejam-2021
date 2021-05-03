2145a4a9c9a9dd4624d1cb9c1503fd1e30992f92
D-RyusukeMatsumoto <matsumoto@dedication.co.jp>
2021-05-03 18:09:35 +0900

    ゾンビ周りのコードをnamespaceで区切った

---
+++ Assets/gw_game_jam/Scripts/Enemy/ZombieBoat.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+namespace gw_game_jam.Enemy
+{
+    /// <summary>
+    /// ゾンビのボート.
+    /// </summary>
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class ZombieBoat : MonoBehaviour
+    {
+        
+        
+        private NavMeshAgent agent;
+  
+        
+        void Start()
+        {
+        
+        }
+
+        
+        void Update()
+        {
+        
+        }
+    }
+}
