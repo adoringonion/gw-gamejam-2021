2309e50aee210f6f44142f7c320a9211732c0c20
Fumihito Morita <adringonion.byazura@gmail.com>
2021-05-04 23:07:41 +0900

    移動

---
+++ Assets/gw_game_jam/Scripts/WhiteShark.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace gw_game_jam.Scripts
+{
+    public class WhiteShark : MonoBehaviour
+    {
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+
+        private void DestroyWhen()
+        {
+        }
+    }
+}
