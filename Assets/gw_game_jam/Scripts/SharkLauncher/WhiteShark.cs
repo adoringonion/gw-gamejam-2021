a1565c3c5f95b1f1736a9e798660f3a5524c6757
Fumihito Morita <adringonion.byazura@gmail.com>
2021-05-04 23:15:15 +0900

    射出後3秒で消えるように

--- Assets/gw_game_jam/Scripts/SharkLauncher/WhiteShark.cs
+++ Assets/gw_game_jam/Scripts/SharkLauncher/WhiteShark.cs
@@ -1,12 +1,17 @@
+using System;
+using UniRx;
 using UnityEngine;
 
-namespace gw_game_jam.Scripts
+namespace gw_game_jam.Scripts.SharkLauncher
 {
     public class WhiteShark : MonoBehaviour
     {
         void Start()
         {
-        
+            Observable.Interval(TimeSpan.FromSeconds(3)).Subscribe(_ =>
+            {
+                Destroy(gameObject);
+            } );
         }
 
         void Update()
