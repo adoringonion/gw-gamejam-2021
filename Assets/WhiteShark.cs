e025706c1c5a6646d55d0c4a9a658a15483b979b
Fumihito Morita <adringonion.byazura@gmail.com>
2021-05-04 22:30:49 +0900

    サメのプレハブから必要なさそうなものを削除

---
+++ Assets/WhiteShark.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhiteShark : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void DestroyWhen()
+    {
+    }
+}
