ea8487cf6ec60d4a7cae288d87d55199f44e793d
Fumihito Morita <adringonion.byazura@gmail.com>
2021-05-05 21:46:52 +0900

    シングルトン生成を修正

--- Assets/gw_game_jam/Scripts/Score/ScoreController.cs
+++ Assets/gw_game_jam/Scripts/Score/ScoreController.cs
@@ -5,25 +5,33 @@ namespace gw_game_jam.Scripts.Score
 {
     public class ScoreController : MonoBehaviour
     {
-        public readonly Subject<int> scoreSubject = new Subject<int>();
+        private readonly Subject<int> scoreSubject = new Subject<int>();
 
-        public static ScoreController instance;
+        private static ScoreController instance;
+
+        public ScoreController Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = this;
+                }
+
+                return instance;
+            }
+        }
 
         [SerializeField] private int currentScore;
 
         private void Awake()
         {
-            if (instance == null)
-            {
-                instance = this;
-                DontDestroyOnLoad(gameObject);
-            }
-            else
-            {
-                Destroy(gameObject);
-            }
-
             scoreSubject.Subscribe(point => currentScore += point);
+        }
+
+        public void AddScore(int score)
+        {
+            scoreSubject.OnNext(score);
         }
     }
 }
