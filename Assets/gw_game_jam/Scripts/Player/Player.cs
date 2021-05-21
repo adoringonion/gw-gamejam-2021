69130b151d584fe89ca337b29485327c034e5dd1
RyusukeMatsumoto <matsumotokakadevelop1102@gmail.com>
2021-05-22 01:05:53 +0900

    入力を修正

--- Assets/gw_game_jam/Scripts/Player/Player.cs
+++ Assets/gw_game_jam/Scripts/Player/Player.cs
@@ -4,6 +4,7 @@ using UniRx;
 
 
 using gw_game_jam.Scripts.SharkLauncher;
+using UniRx.Triggers;
 
 namespace gw_game_jam.Player
 {
@@ -17,8 +18,8 @@ namespace gw_game_jam.Player
         
         private void Start()
         {
-            Observable.EveryUpdate().Where(_ => OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
-                .ThrottleFirst(TimeSpan.FromSeconds(0.5)).Subscribe(_ => launcherController.Fire());
+            this.UpdateAsObservable().Where(_ => OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
+                .ThrottleFirst(TimeSpan.FromSeconds(0.5)).Subscribe(_ => launcherController.Fire()).AddTo(this);
         }
 
     }
